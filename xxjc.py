3dc3fb33de7bd75f236c18ce2b5e57be4a76e0eb
dawn <dawn@katwczn.com>
2021-05-17 04:28:24 +0800

    优化速度

--- xxjc.py
+++ xxjc.py
@@ -1,6 +1,7 @@
 import requests,json,re,time,os,random,string,keyboard,pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 from PIL import Image
 from pytesseract import image_to_string
@@ -23,13 +24,12 @@ driver.get("https://outlook.live.com/owa/?nlp=1&signup=1")
 login_form = driver.find_element_by_id('MemberName')
 login_form.send_keys('a' + salt )
 login_click = driver.find_element_by_id('iSignupAction').click()
-sleep(3.5)
+driver.implicitly_wait(3.5)
 login_pass = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr  Chrome _Win _M90 _D0 Full Win81 RE_WebKit hide-cookie-banner']/div[@id='iPageElt']/div[@id='c_base']/div[@id='c_content']/div[@class='outer']/div[@class='middle ']/div[@id='inner']/div[@class='win-scroll']/div[@id='pageContent']/div[@id='maincontent']/div[@id='pageControlHost']/div[@class='pagination-view has-identity-banner']/div[@id='Password']/form[@id='PasswordForm']/div[@class='row form-group']/div[@class='col-md-24']/input[@id='PasswordInput']")
 login_pass.send_keys(email_pass)
 del login_click
 login_click = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr  Chrome _Win _M90 _D0 Full Win81 RE_WebKit hide-cookie-banner']/div[@id='iPageElt']/div[@id='c_base']/div[@id='c_content']/div[@class='outer']/div[@class='middle ']/div[@id='inner']/div[@class='win-scroll']/div[@id='pageContent']/div[@id='maincontent']/div[@id='pageControlHost']/div[@class='pagination-view has-identity-banner']/div[@id='Password']/form[@id='PasswordForm']/div[@class='win-button-pin-bottom']/div[@class='row']/div[@class='button-container']/div[@class='inline-block']/input[@id='iSignupAction']").click()
 del login_click,login_form,login_pass
-sleep(1.5)
 login_form = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr  Chrome _Win _M90 _D0 Full Win81 RE_WebKit hide-cookie-banner']/div[@id='iPageElt']/div[@id='c_base']/div[@id='c_content']/div[@class='outer']/div[@class='middle ']/div[@id='inner']/div[@class='win-scroll']/div[@id='pageContent']/div[@id='maincontent']/div[@id='pageControlHost']/div[@class='pagination-view has-identity-banner']/div[@id='ProfileAccrual']/form[@id='ProfileAccrualForm']/div[@id='ProfileAccrualInputPane']/div[@class='row']/div[@class='form-group col-md-24'][1]/input[@id='LastName']")
 login_form.send_keys('1')
 del login_form
@@ -37,7 +37,6 @@ login_form = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr
 login_form.send_keys('1')
 login_form.send_keys(Keys.ENTER)
 del login_form
-sleep(1.5)
 try:
     login_form = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr  Chrome _Win _M90 _D0 Full Win81 RE_WebKit hide-cookie-banner']/div[@id='iPageElt']/div[@id='c_base']/div[@id='c_content']/div[@class='outer']/div[@class='middle ']/div[@id='inner']/div[@class='win-scroll']/div[@id='pageContent']/div[@id='maincontent']/div[@id='pageControlHost']/div[@class='pagination-view has-identity-banner']/div[@id='BirthDateCountryAccrual']/form[@id='BirthDateCountryAccrualForm']/div[@id='BirthDateCountryAccrualInputPane']/div[@class='row'][2]/div[@class='form-group form-control force-padding']/div[@id='BirthYearContainer']/input[@id='BirthYear']")
     login_form.send_keys('1980')
@@ -59,9 +58,9 @@ del login_form
 login_form = driver.find_element_by_xpath("/html[@class='m_ul']/body[@class='ltr  Chrome _Win _M90 _D0 Full Win81 RE_WebKit hide-cookie-banner']/div[@id='iPageElt']/div[@id='c_base']/div[@id='c_content']/div[@class='outer']/div[@class='middle ']/div[@id='inner']/div[@class='win-scroll']/div[@id='pageContent']/div[@id='maincontent']/div[@id='pageControlHost']/div[@class='pagination-view has-identity-banner']/div[@id='BirthDateCountryAccrual']/form[@id='BirthDateCountryAccrualForm']/div[@id='BirthDateCountryAccrualInputPane']/div[@class='win-button-pin-bottom']/div[@class='row']/div[@class='button-container no-margin-bottom']/div[@class='inline-block']/input[@id='iSignupAction']").click()
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 def send_verification_code():
-    htmlcookie = requests.get('https://xxjc.work',headers=headers,verify = False)
+    htmlcookie = requests.get('https://xxjc.vip',headers=headers,verify = False)
     c = {'email':email_name}
-    requests.post('https://xxjc.work/auth/send',data=c,headers=headers,cookies=htmlcookie.cookies,verify = False)
+    requests.post('https://xxjc.vip/auth/send',data=c,headers=headers,cookies=htmlcookie.cookies,verify = False)
 print('OK!')
 keyboard.wait(']')
 send_verification_code()
@@ -77,10 +76,10 @@ if text == '小小机场' :
 '''
 def login():
     d = {'email':email_name,'name':email_name,'passwd':email_name,'repasswd':email_name,'wechat':email_name,'imtype':'1','code':'0','emailcode':z}
-    requests.post('https://xxjc.work/auth/register',headers=headers,data=d,verify = False)
+    requests.post('https://xxjc.vip/auth/register',headers=headers,data=d,verify = False)
     s = {'email':email_name,'passwd':email_name,'code':''}
-    f = requests.post('https://xxjc.work/auth/login',headers=headers,data=s,verify = False)
-    i = requests.get('https://xxjc.work/user',headers=headers,cookies=f.cookies,verify = False)
+    f = requests.post('https://xxjc.vip/auth/login',headers=headers,data=s,verify = False)
+    i = requests.get('https://xxjc.vip/user',headers=headers,cookies=f.cookies,verify = False)
     html = i.text
     url = re.findall('<button class="copy-text btn btn-subscription col-xx-12 col-sm-3 col-lg-2" type="button" data-clipboard-text="(.*?)">',html)
     ssrjson = url[0]
