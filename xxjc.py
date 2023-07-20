3c1ea21ed09bc0d26ebe49df57cb4ff4ae0cfd4d
dawn23666 <dawn@katwczn.com>
2023-07-21 03:50:06 +0800

    refactor:Headers简化

--- xxjc.py
+++ xxjc.py
@@ -2,9 +2,7 @@ import pyperclip
 import re
 import requests
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 '
-                  'Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0'}
 email = input('email:')
 verification_code = {'email': email}
 url = 'https://xxjc.in/'
